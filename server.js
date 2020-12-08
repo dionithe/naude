9802dd3841fc9fe3902bc1c9ec62d588fc966546
Theo <theo@dionisi.ovh>
2020-12-08 15:05:41 +0100

    init server

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+let port = 3000;
+
+app.listen(port, () => {
+    console.log('Le serveur est en route');
+    console.loge(`Server listening at http:://localhost:${port}`);
+});
+
+app.get('/', (req, res, next) => {
+    res.send('Bonjour !');
+});
