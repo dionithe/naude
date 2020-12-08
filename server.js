e6d3c96c9ee3aaf87edc16dede7c480009a247d9
Theo <theo@dionisi.ovh>
2020-12-08 15:11:33 +0100

    correction

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let port = 3000;
 
 app.listen(port, () => {
     console.log('Le serveur est en route');
-    console.loge(`Server listening at http:://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
 
 app.get('/', (req, res, next) => {
